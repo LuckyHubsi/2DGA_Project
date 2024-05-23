ae36771fa342aa4a80371d1773cbe1759d51d8c8
Marco Huber <cc221040@fhstp.ac.at>
2024-05-23 17:09:22 +0200

    Animations (kinda) work... FINALLY

--- Assets/Scripts/Spine/PlayerCharacterModel.cs
+++ Assets/Scripts/Spine/PlayerCharacterModel.cs
@@ -1,119 +1,109 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-
-/// <summary>
-/// This is the M(odel) Part of the MVC Model
-/// This class is responsible for all our characters functionality
-/// as well as keeping track in which state the character currently is
-/// </summary>
+using Spine.Unity;
 
 public class PlayerCharacterModel : MonoBehaviour
 {
     #region variables
     public PlayerState state;
     public bool facingLeft;
+    public PlayerCharacterView view; // Reference to the view
+
+    public float moveSpeed = 5f; // Adjust this speed according to your needs
     #endregion
 
     #region jump
-    public void TryJump() {
-        StartCoroutine(JumpRoutine());
+    public void TryJump()
+    {
+        if (state != PlayerState.Jumping)
+        {
+            StartCoroutine(JumpRoutine());
+        }
     }
 
-    IEnumerator JumpRoutine() {
-        if (state == PlayerState.Jumping) {
-            yield break;
-        }
-
-
+    IEnumerator JumpRoutine()
+    {
         state = PlayerState.Jumping;
 
         // Start the animation immediately
-        float animationDuration = 0.3f; 
+        float animationDuration = 0.3f;
         float timer = 0f;
         while (timer < animationDuration)
         {
-            // Update animation here
             timer += Time.deltaTime;
             yield return null;
         }
 
-        // Delayed movement
-        float delayTime = 0.3f; 
+        float delayTime = 0.3f;
         yield return new WaitForSeconds(delayTime);
 
-        // Horizontal movement while jumping
-        float horizontalSpeed = 3f; 
-        float horizontalDirection = Input.GetAxis("Horizontal"); 
-        Vector3 horizontalMovement = new Vector3(horizontalDirection * horizontalSpeed * Time.deltaTime, 0f, 0f);
-        transform.Translate(horizontalMovement);
-
-        // our starting position in local space
-        Vector3 position = transform.localPosition;
-        // how long the jumo will take
         float jumpTime = 1.7f;
         float half = jumpTime * 0.5f;
-        // the power of the jump, changes the jumpheight
         float jumpPower = 7f;
 
-        // this is the first half of the jump, our character is getting propelled upwards
-        for (float t = 0; t < half; t += Time.deltaTime) {
-            // we calculate the power increment for the given frame
-            // the power increment grows smaller with the t getting larger over time
+        for (float t = 0; t < half; t += Time.deltaTime)
+        {
             float powerIncrement = jumpPower * (half - t);
-            // move the characters translate by the calculated power increment upwards each frame
             transform.Translate((powerIncrement * Time.deltaTime) * Vector3.up);
-            // start next loop
+
+            float horizontalDirection = Input.GetAxis("Horizontal");
+            Vector3 horizontalMovement = new Vector3(horizontalDirection * moveSpeed * Time.deltaTime, 0f, 0f);
+            transform.Translate(horizontalMovement);
+
+            if (horizontalDirection != 0)
+            {
+                facingLeft = horizontalDirection < 0;
+            }
+
             yield return null;
         }
-        // this is the second half of the jump, which starts then the character reaches the highest point of the jump
-        // our character is getting propelled downwards
-        for (float t = 0; t < half; t += Time.deltaTime) {
-            /* we calculate the power increment for the given frame
-             * the power increment grows larger with the t getting larger over time
-             * we want the power to get smaller to simulate our character getting dragged to the ground by gravity */
+
+        for (float t = 0; t < half; t += Time.deltaTime)
+        {
             float powerIncrement = jumpPower * t;
-            // move the characters translate by the calculated power increment downward each frame
             transform.Translate((powerIncrement * Time.deltaTime) * Vector3.down);
-            // start next loop
+
+            float horizontalDirection = Input.GetAxis("Horizontal");
+            Vector3 horizontalMovement = new Vector3(horizontalDirection * moveSpeed * Time.deltaTime, 0f, 0f);
+            transform.Translate(horizontalMovement);
+
+            if (horizontalDirection != 0)
+            {
+                facingLeft = horizontalDirection < 0;
+            }
+
             yield return null;
         }
-        // reset the character to starting position
-        transform.localPosition = position;
 
         state = PlayerState.Idle;
     }
     #endregion
 
     #region movement
-    public float moveSpeed = 5f; // Adjust this speed according to your needs
-
-    public void TryMove(float direction) {
-
-        if (state != PlayerState.Jumping)
+    public void TryMove(float direction)
+    {
+        if (state != PlayerState.Attacking && state != PlayerState.Shielding)
         {
-            state = (direction == 0) ? PlayerState.Idle : PlayerState.Running;
+            if (state != PlayerState.Jumping)
+            {
+                state = (direction == 0) ? PlayerState.Idle : PlayerState.Running;
+            }
 
-            // Move the character
             Vector3 movement = new Vector3(direction * moveSpeed * Time.deltaTime, 0f, 0f);
             transform.Translate(movement);
 
-            // Adjust facing direction
             if (direction != 0)
             {
-                bool facingLeft = direction < 0f;
-                this.facingLeft = facingLeft;
+                facingLeft = direction < 0f;
             }
         }
     }
     #endregion
 
     #region attack
-
     public void TryAttack()
     {
-        if (state != PlayerState.Attacking && state != PlayerState.Jumping)
+        if (state != PlayerState.Attacking && state != PlayerState.Shielding && state != PlayerState.Jumping)
         {
             StartCoroutine(AttackRoutine());
         }
@@ -123,21 +113,20 @@ public class PlayerCharacterModel : MonoBehaviour
     {
         state = PlayerState.Attacking;
 
-        // Implement your attack logic here
         Debug.Log("Attacking...");
 
         yield return new WaitForSeconds(0.7f); // Adjust the attack duration
 
+        //yield return new WaitForSpineAnimationComplete(view.skeletonAnimation.AnimationState.GetCurrent(0));
+
         state = PlayerState.Idle;
     }
-
     #endregion
 
     #region shield
-
     public void TryShield()
     {
-        if (state != PlayerState.Shielding && state != PlayerState.Jumping)
+        if (state != PlayerState.Shielding && state != PlayerState.Attacking && state != PlayerState.Jumping)
         {
             StartCoroutine(ShieldRoutine());
         }
@@ -147,17 +136,16 @@ public class PlayerCharacterModel : MonoBehaviour
     {
         state = PlayerState.Shielding;
 
-        // Implement your shield logic here
         Debug.Log("Shielding...");
 
-        yield return new WaitForSeconds(3f); // Adjust the shield duration
+        yield return new WaitForSpineAnimationComplete(view.skeletonAnimation.AnimationState.GetCurrent(0));
 
         state = PlayerState.Idle;
     }
-
     #endregion
 
-    public enum PlayerState { 
+    public enum PlayerState
+    {
         Idle,
         Running,
         Jumping,
--- Assets/Scripts/Spine/PlayerCharacterView.cs
+++ Assets/Scripts/Spine/PlayerCharacterView.cs
@@ -1,12 +1,5 @@
 using Spine.Unity;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-/// <summary>
-/// This is the V(iew) Part of the MVC Model
-/// This class is responsible for changing the animations/sprites/looks of the character
-/// </summary>
 
 public class PlayerCharacterView : MonoBehaviour
 {
@@ -19,55 +12,63 @@ public class PlayerCharacterView : MonoBehaviour
     PlayerCharacterModel.PlayerState previousState;
     #endregion
 
-    // Update is called once per frame
+    void Start()
+    {
+        model.view = this; // Assign this view to the model
+    }
+
     void Update()
     {
-        // flip the character if current direction does not equal the facing direction from the model
-        if (skeletonAnimation.Skeleton.ScaleX < 0 != model.facingLeft) {
+        if (skeletonAnimation.Skeleton.ScaleX < 0 != model.facingLeft)
+        {
             Turn(model.facingLeft);
         }
 
-        // the new current PlayerState is read from the model
         PlayerCharacterModel.PlayerState currentState = model.state;
 
-        // if the state in the previous loop is not matching the current one we need to adjust to a new animation
-        if (previousState != currentState) { 
+        if (previousState != currentState)
+        {
             PlayNewAnimation();
         }
 
-        // the previous state will be the current state in the next loop
         previousState = currentState;
     }
 
-    // sets a new antimation to be played depending on the current PlayerState
-    void PlayNewAnimation() {
+    void PlayNewAnimation()
+    {
         PlayerCharacterModel.PlayerState newstate = model.state;
         Spine.Animation nextAnimation;
+        bool shouldLoop;
 
         switch (newstate)
         {
             case PlayerCharacterModel.PlayerState.Jumping:
                 nextAnimation = jump;
+                shouldLoop = false;
                 break;
             case PlayerCharacterModel.PlayerState.Running:
                 nextAnimation = run;
+                shouldLoop = true;
                 break;
             case PlayerCharacterModel.PlayerState.Shielding:
                 nextAnimation = shield;
+                shouldLoop = false;
                 break;
             case PlayerCharacterModel.PlayerState.Attacking:
                 nextAnimation = attack;
+                shouldLoop = false;
                 break;
             default:
                 nextAnimation = idle;
+                shouldLoop = true;
                 break;
         }
 
-        // set a new animation for the character immediatelly
-        skeletonAnimation.AnimationState.SetAnimation(0, nextAnimation, true);
+        skeletonAnimation.AnimationState.SetAnimation(0, nextAnimation, shouldLoop);
     }
 
-    public void Turn(bool facingLeft) {
+    public void Turn(bool facingLeft)
+    {
         skeletonAnimation.Skeleton.ScaleX = facingLeft ? -1 : 1f;
     }
 }
